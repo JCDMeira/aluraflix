1ed43d065396219c9837379f93344d05f06bc77a
Jean Meira <jean.meira10@hotmail.com>
2020-07-28 17:02:49 -0300

    Atualização de conteúdo e adição de telas de cadastro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import {BrowserRouter,Switch , Route} from "react-router-dom";
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
+  /*
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
+  */
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>  
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>  
+      <Route path="/cadastro/Categoria" component={CadastroCategoria} exact/> 
+      <Route component={() => (<div> Página 404</div>) } exact/>  
+    </Switch>  
+  </BrowserRouter>,
   document.getElementById('root')
 );
